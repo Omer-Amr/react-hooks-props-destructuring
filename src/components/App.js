47304f441ac785bf0efd062f089050d81fd5495a
omer <you@example.com>
2023-01-27 18:28:25 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <MovieCard title={title} genres={genresArr} />
+      {/* <MovieCard title={"Example"} genres={["action", "Adventure"]} posterSrc={posterURL}/> */}
     </div>
   );
 }
